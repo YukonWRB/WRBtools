91fb1583c0878d530916ca79a93232535231c91b
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2023-02-05 23:28:59 +0000

    New function to check and upadate HYDAT DB.

--- R/hydroConnect.R
+++ R/hydroConnect.R
@@ -1,6 +1,6 @@
 #' Connect to hydrology database
 #'
-#' This function exists to facilitate connecting to the hydrology database, especially if the database type and connection method changes in the future.
+#' This function exists to facilitate connecting to the hydrology database, especially if the database type and connection method changes in the future. Cannot be used to create a new database, use DBI::dbConnect with the appropriate driver (e.g. RSQlite::SQLite()) for that purpose.
 #'
 #' @param path The path to the database. Currently supports only .sqlite databases.
 #' @param timeout For sqlite DBs, the duration in which to retry an operation in milliseconds. Valid for the duration of the connection.
@@ -12,7 +12,7 @@
 hydroConnect <- function(path, timeout = 100000){
 
   if(!file.exists(path)){
-    stop("The path you specified either does not exist or this computer does not have access to that drive.")
+    stop("The path you specified either does not exist or this computer does not have access to that drive. Read the function help file if you're trying to create a new DB.")
   }
 
   tryCatch({
