114999e295e6748be870704003bfaa405de336e7
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2023-05-24 04:59:36 +0000

    New plotting function dramatically streamlines the functions in WRBfloods to plot level/flow data. Much more flexibility on where plots are zoomed and how returns are calculated.

--- development_files/scripts/data_create.R
+++ development_files/scripts/data_create.R
@@ -1,7 +1,11 @@
 #Shortcut script for (re)creating sysdata.rda
 
 prov_buff <- sf::read_sf(dsn = "G:/water/Common_GW_SW/Data/basins/Clip_polygons", layer = "Provinces_buffered_300km")
+level_returns_max <- read.csv("data-raw/level_returns_max.csv")
 
-data <- list(prov_buff = prov_buff)
+#NOTE: the name of *_returns tables be formulated so that the first part of the name matches the parameter as it's spelled in the local database, followed by _returns. If in doubt, check the parameter names using DB_browse_ts.
+
+data <- list(prov_buff = prov_buff,
+             level_returns_max = level_returns_max)
 
 usethis::use_data(data, internal=TRUE, overwrite=TRUE)
